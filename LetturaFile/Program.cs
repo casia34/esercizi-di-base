8d8802d7488cbd8b79d7b10f7ad8c8e6b689c9b8
Betacom <Betacom@BETACOM-PCN9L2>
2022-10-22 16:49:20 +0200

    Aggiungere i file di progetto.

---
+++ LetturaFile/Program.cs
@@ -0,0 +1,69 @@
+﻿using System.Reflection.PortableExecutable;
+
+namespace LetturaFile
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region primo
+
+            /*  OpenFile Utilizzo_Metodo = new OpenFile();
+              Console.WriteLine("srvivi il nome del file");
+              string stamp = Console.ReadLine();
+              bool flag = true;
+              bool flag1 = true;
+              try
+              {
+                  do
+                  {
+                      if (Utilizzo_Metodo.FileEsiste($@"C:\Users\Betacom\Desktop\{stamp}"))
+                      {
+                          string stampa = Utilizzo_Metodo.openFile($@"C:\Users\Betacom\Desktop\{stamp}");
+                          Console.WriteLine(stampa);
+
+                          Console.WriteLine("il file è stato trovao ed aperto, ne vuoi aprire un'altro? (S/N)");
+                          while (flag1)
+                          {
+                              string risp = Console.ReadLine();
+                              if (risp == "N")
+                              {
+                                  flag = false;
+                                  break;
+                              }
+                              else if (risp == "S")
+                              {
+                                  Console.WriteLine("Scrivi un'altro nome file");
+                                  stamp = Console.ReadLine();
+                                  break;
+                              }
+                              else
+                              {
+                                  Console.WriteLine(" comando non valido");
+                                  stamp = Console.ReadLine();
+                                  break;
+                              }
+                          }
+                      }
+                      else
+                      {
+                          Console.WriteLine("Il file non è stato Trovato, scrivi Nuovamente il nome");
+                          stamp = Console.ReadLine();
+                          throw new Exception(" l'utente non riesce a trovare il file:"+ stamp);
+                      }
+                  } while (flag != null);
+              }
+              catch (Exception Ex)
+              {
+                  Console.WriteLine(Ex.Message);
+                  throw;
+              }
+               */
+            #endregion
+
+            Split_Document split_Document = new Split_Document();
+
+            split_Document.Document("Cars.txt");
+        }
+    }
+}
---
+++ LetturaFile/Split_Document.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LetturaFile
+{
+    public class Split_Document
+    {
+        string Stamp = "";
+        public void Equals()
+        {
+
+        }
+        public void Document(string stamp)
+        {
+            Stamp = stamp;
+
+            try
+            {
+                Console.WriteLine($"{Stamp}.txt");
+
+                if (File.Exists($@"C:\Users\Betacom\Desktop\{Stamp}.txt"))
+                {
+                    Console.WriteLine(" il file è stato trovato: il contenuto del file é:\n");
+                    string[] macchina = File.ReadAllLines($@"C:\Users\Betacom\Desktop\{stamp}.txt");
+                    string[] car = new string[2];
+
+
+                    foreach (string dati_array in macchina)
+                    {
+
+                        int j = 1;
+                        car = dati_array.Split(":");
+
+                        
+                        foreach (string car_macchina in car)
+                        {
+                            if (j == 1)
+                            {
+                                Console.WriteLine(" Marca " + car_macchina + "\n");
+                                j++;
+                            }
+                            else if (j == 2)
+                            {
+                                Console.WriteLine(" Modello " + car_macchina + "\n");
+                                j++;
+                            }
+                            else if (j == 3)
+                            {
+                                Console.WriteLine(" Cilindrata " + car_macchina + "\n");
+                                j = 0;
+                            }
+
+                        }
+
+                        Console.WriteLine("vuoi Cercare un nuovo File?");
+
+                    }
+
+                }
+                else if (File.Exists($@"C:\Users\Betacom\Desktop\{Stamp}"))
+                {
+                    Console.WriteLine(" il file è stato trovato: il contenuto del file é:\n");
+                    string[] macchina = File.ReadAllLines($@"C:\Users\Betacom\Desktop\{stamp}");
+                    string[] car = new string[2];
+
+
+                    foreach (string dati_array in macchina)
+                    {
+
+                        int j = 1;
+                        car = dati_array.Split(":");
+
+                        Console.WriteLine(j);
+                        foreach (string car_macchina in car)
+                        {
+                            if (j == 1)
+                            {
+                                Console.WriteLine(" Marca " + car_macchina + "\n");
+                                j++;
+                            }
+                            else if (j == 2)
+                            {
+                                Console.WriteLine(" Modello " + car_macchina + "\n");
+                                j++;
+                            }
+                            else if (j == 3)
+                            {
+                                Console.WriteLine(" Cilindrata " + car_macchina + "\n");
+                                j = 0;
+                            }
+
+                        }
+
+                    }
+
+                }
+                else
+                {
+                    Console.WriteLine("il file non è stato trovato controlla l'estensione o il nome");
+                    throw new Exception("l'utente non riesce a trovare il file");
+                }
+
+
+            }
+            catch (Exception Ex)
+            {
+                Console.WriteLine(Ex.Message);
+
+            }
+        }
+
+       
+    }
+}
