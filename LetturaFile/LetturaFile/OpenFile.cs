4cd0ccf2079fe7809565b1b21029699d7c5b6175
casia34 <110932011+casia34@users.noreply.github.com>
2022-10-22 16:52:44 +0200

    Add files via upload

---
+++ LetturaFile/LetturaFile/OpenFile.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LetturaFile
+{
+    public class OpenFile
+    {
+        string M; // Nome
+        string[] arra = new string[0];
+
+        public bool FileEsiste(string m)
+        {
+            M = m;
+
+            bool Variabile = File.Exists($@"{M}.txt");
+
+            return Variabile;
+        }
+
+        public string openFile(string m )
+        {
+            M = m;
+          
+           string Variabile = File.ReadAllText($@"{M}.txt");
+
+            return Variabile;
+        }
+
+       
+
+        public void openFile()
+        {
+           
+        }
+    }
+}
